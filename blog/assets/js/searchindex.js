196f972d45e911ab8ad2205fc194515a87417e15
$env:op_build_user <$env:op_build_user_email>
2018-06-19 16:30:00 +0000

    Commit from AppVeyor

---
+++ blog/assets/js/searchindex.js
@@ -0,0 +1,42 @@
+
+var searchModule = function() {
+    var idMap = [];
+    function y(e) { 
+        idMap.push(e); 
+    }
+    var idx = lunr(function() {
+        this.field('title', { boost: 10 });
+        this.field('content');
+        this.field('description', { boost: 5 });
+        this.field('tags', { boost: 50 });
+        this.ref('id');
+
+        this.pipeline.remove(lunr.stopWordFilter);
+        this.pipeline.remove(lunr.stemmer);
+    });
+    function a(e) { 
+        idx.add(e); 
+    }
+
+    a({
+        id:0,
+        title:"Test Service with Search",
+        content:"test service try the search here",
+        description:"Welcome",
+        tags:'Where I Live'
+    });
+
+    y({
+        url:'/blog/services/test-service',
+        title:"Test Service with Search",
+        description:"Welcome!"
+    });
+
+    return {
+        search: function(q) {
+            return idx.search(q).map(function(i) {
+                return idMap[i.ref];
+            });
+        }
+    };
+}();
