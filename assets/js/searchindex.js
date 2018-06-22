02b12fb1b6b05f71e71c5ab111cba9e2da9b45b6
$env:op_build_user <$env:op_build_user_email>
2018-06-22 10:40:28 +0000

    Commit from AppVeyor

--- assets/js/searchindex.js
+++ assets/js/searchindex.js
@@ -20,14 +20,6 @@ var searchModule = function() {
 
     a({
         id:0,
-        title:"John Hair Salon",
-        content:"Hair Salon",
-        description:"The best John Hair Salon in Krakow Centrum",
-        tags:'Krakow Centrum Hair Hair'
-    });
-
-    a({
-        id:1,
         title:"Mark Car Rental",
         content:"Car Rental",
         description:"The Best Mark Car Rental in Krakow Kazimierz",
@@ -35,17 +27,19 @@ var searchModule = function() {
     });
 
     a({
-        id:2,
+        id:1,
         title:"Dave Car Rental",
         content:"additional search tags",
         description:"The Best Dave Car Rental in Krakow Bronowice",
         tags:'Krakow Bronowice Car Car'
     });
 
-    y({
-        url:'/BigDoorWyamBlog/services/johns-hair-salon',
-        title:"John's Hair Salon",
-        description:"The best John's Hair Salon in Krakow Centrum"
+    a({
+        id:2,
+        title:"John Hair Salon",
+        content:"Hair Salon",
+        description:"The best John Hair Salon in Krakow Centrum",
+        tags:'Krakow Centrum Hair Hair'
     });
 
     y({
@@ -58,6 +52,12 @@ var searchModule = function() {
         url:'/BigDoorWyamBlog/services/daves-car-rental',
         title:"Dave's Car Rental",
         description:"The Best Dave's Car Rental in Krakow, Bronowice"
+    });
+
+    y({
+        url:'/BigDoorWyamBlog/services/johns-hair-salon',
+        title:"John's Hair Salon",
+        description:"The best John's Hair Salon in Krakow Centrum"
     });
 
     return {
