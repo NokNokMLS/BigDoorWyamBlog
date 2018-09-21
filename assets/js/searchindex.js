c642b819e903908a6f04f77f291f27c2e9f5b03b
$env:op_build_user <$env:op_build_user_email>
2018-09-21 16:13:27 +0000

    Commit from AppVeyor

--- assets/js/searchindex.js
+++ assets/js/searchindex.js
@@ -10,20 +10,6 @@ var searchModule = function() {
     a(
         {
             id:0,
-            title:"Dave Car Rental",
-            content:"additional search tags",
-            description:"Best Dave Car Rental Krakow Bronowice",
-            tags:'Kraków Bronowice Car Car'
-        },
-        {
-            url:'/BigDoorWyamBlog/services/daves-car-rental',
-            title:"Dave's Car Rental",
-            description:"The Best Dave's Car Rental in Krakow, Bronowice"
-        }
-    );
-    a(
-        {
-            id:1,
             title:"Food shop Bronowice",
             content:"pierogi",
             description:"Polish food shop pierogi",
@@ -37,7 +23,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:2,
+            id:1,
             title:"Mark Car Rental",
             content:"Car Rental",
             description:"Best Mark Car Rental Krakow Kazimierz",
@@ -47,6 +33,20 @@ var searchModule = function() {
             url:'/BigDoorWyamBlog/services/marks-car-rental',
             title:"Mark's Car Rental",
             description:"The Best Mark's Car Rental in Krakow, Kazimierz"
+        }
+    );
+    a(
+        {
+            id:2,
+            title:"Dave Car Rental",
+            content:"additional search tags",
+            description:"Best Dave Car Rental Krakow Bronowice",
+            tags:'Kraków Bronowice Car Car'
+        },
+        {
+            url:'/BigDoorWyamBlog/services/daves-car-rental',
+            title:"Dave's Car Rental",
+            description:"The Best Dave's Car Rental in Krakow, Bronowice"
         }
     );
     a(
