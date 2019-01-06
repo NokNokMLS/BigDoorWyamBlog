56826fc1c7fa63710bbc0cd70900d7deaab33350
$env:op_build_user <$env:op_build_user_email>
2019-01-06 09:03:54 +0000

    Commit from AppVeyor

--- assets/js/searchindex.js
+++ assets/js/searchindex.js
@@ -10,20 +10,6 @@ var searchModule = function() {
     a(
         {
             id:0,
-            title:"Food shop Bronowice",
-            content:"pierogi",
-            description:"Polish food shop pierogi",
-            tags:'Krakow Bronowice Food Food'
-        },
-        {
-            url:'/services/a-food-shop-in-bronowice',
-            title:"A Food shop in Bronowice",
-            description:"Polish food shop with pierogi"
-        }
-    );
-    a(
-        {
-            id:1,
             title:"Dave Car Rental",
             content:"additional search tags",
             description:"Best Dave Car Rental Krakow Bronowice",
@@ -37,21 +23,21 @@ var searchModule = function() {
     );
     a(
         {
-            id:2,
-            title:"John Hair Salon",
-            content:"Hair Salon",
-            description:"best John Hair Salon Krakow Centrum",
-            tags:'Krakow Centrum Hair Hair'
+            id:1,
+            title:"Food shop Bronowice",
+            content:"pierogi",
+            description:"Polish food shop pierogi",
+            tags:'Krakow Bronowice Food Food'
         },
         {
-            url:'/services/johns-hair-salon',
-            title:"John's Hair Salon",
-            description:"The best John's Hair Salon in Krakow Centrum"
+            url:'/services/a-food-shop-in-bronowice',
+            title:"A Food shop in Bronowice",
+            description:"Polish food shop with pierogi"
         }
     );
     a(
         {
-            id:3,
+            id:2,
             title:"Mark Car Rental",
             content:"Car Rental",
             description:"Best Mark Car Rental Krakow Kazimierz",
@@ -61,6 +47,20 @@ var searchModule = function() {
             url:'/services/marks-car-rental',
             title:"Mark's Car Rental",
             description:"The Best Mark's Car Rental in Krakow, Kazimierz"
+        }
+    );
+    a(
+        {
+            id:3,
+            title:"John Hair Salon",
+            content:"Hair Salon",
+            description:"best John Hair Salon Krakow Centrum",
+            tags:'Krakow Centrum Hair Hair'
+        },
+        {
+            url:'/services/johns-hair-salon',
+            title:"John's Hair Salon",
+            description:"The best John's Hair Salon in Krakow Centrum"
         }
     );
     var idx = lunr(function() {
