a7fd2f300add516948ca314677b84311ba21225c
$env:op_build_user <$env:op_build_user_email>
2019-01-24 13:25:36 +0000

    Commit from AppVeyor

--- assets/js/searchindex.js
+++ assets/js/searchindex.js
@@ -24,20 +24,6 @@ var searchModule = function() {
     a(
         {
             id:1,
-            title:"Mark Car Rental",
-            content:"Car Rental",
-            description:"Best Mark Car Rental Krakow Kazimierz",
-            tags:'Krakow Kazimierz Car Car'
-        },
-        {
-            url:'/services/marks-car-rental',
-            title:"Mark's Car Rental",
-            description:"The Best Mark's Car Rental in Krakow, Kazimierz"
-        }
-    );
-    a(
-        {
-            id:2,
             title:"John Hair Salon",
             content:"Hair Salon",
             description:"best John Hair Salon Krakow Centrum",
@@ -51,7 +37,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:3,
+            id:2,
             title:"Dave Car Rental",
             content:"additional search tags",
             description:"Best Dave Car Rental Krakow Bronowice",
@@ -61,6 +47,20 @@ var searchModule = function() {
             url:'/services/daves-car-rental',
             title:"Dave's Car Rental",
             description:"The Best Dave's Car Rental in Krakow, Bronowice"
+        }
+    );
+    a(
+        {
+            id:3,
+            title:"Mark Car Rental",
+            content:"Car Rental",
+            description:"Best Mark Car Rental Krakow Kazimierz",
+            tags:'Krakow Kazimierz Car Car'
+        },
+        {
+            url:'/services/marks-car-rental',
+            title:"Mark's Car Rental",
+            description:"The Best Mark's Car Rental in Krakow, Kazimierz"
         }
     );
     var idx = lunr(function() {
