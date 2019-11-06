316328b8813bc5d2318fa2b3757dddc86a0e0637
$env:op_build_user <$env:op_build_user_email>
2019-11-06 19:46:43 +0000

    Commit from AppVeyor

--- assets/js/searchindex.js
+++ assets/js/searchindex.js
@@ -10,34 +10,20 @@ var searchModule = function() {
     a(
         {
             id:0,
-            title:"Polish food shop pierogi",
-            content:"pierogi",
-            description:"Polish food shop pierogi",
-            tags:'Krakow Bronowice Food Food'
+            title:"new title",
+            content:"Car Rental",
+            description:"Best Mark Car Rental Krakow Kazimierz",
+            tags:'Krakow Kazimierz Car Car'
         },
         {
-            url:'/services/a-food-shop-in-bronowice',
-            title:"Polish food shop with pierogi",
-            description:"Polish food shop with pierogi"
+            url:'/services/marks-car-rental',
+            title:"new title",
+            description:"The Best Mark's Car Rental in Krakow, Kazimierz"
         }
     );
     a(
         {
             id:1,
-            title:"John Hair Salon",
-            content:"Hair Salon",
-            description:"best John Hair Salon Krakow Centrum",
-            tags:'Krakow Centrum Hair Hair'
-        },
-        {
-            url:'/services/johns-hair-salon',
-            title:"John's Hair Salon",
-            description:"The best John's Hair Salon in Krakow Centrum"
-        }
-    );
-    a(
-        {
-            id:2,
             title:"ab",
             content:"additional search tags",
             description:"Best Dave Car Rental Krakow Bronowice",
@@ -51,16 +37,30 @@ var searchModule = function() {
     );
     a(
         {
-            id:3,
-            title:"new title",
-            content:"Car Rental",
-            description:"Best Mark Car Rental Krakow Kazimierz",
-            tags:'Krakow Kazimierz Car Car'
+            id:2,
+            title:"John Hair Salon",
+            content:"Hair Salon",
+            description:"best John Hair Salon Krakow Centrum",
+            tags:'Krakow Centrum Hair Hair'
         },
         {
-            url:'/services/marks-car-rental',
-            title:"new title",
-            description:"The Best Mark's Car Rental in Krakow, Kazimierz"
+            url:'/services/johns-hair-salon',
+            title:"John's Hair Salon",
+            description:"The best John's Hair Salon in Krakow Centrum"
+        }
+    );
+    a(
+        {
+            id:3,
+            title:"Polish food shop pierogi",
+            content:"pierogi",
+            description:"Polish food shop pierogi",
+            tags:'Krakow Bronowice Food Food'
+        },
+        {
+            url:'/services/a-food-shop-in-bronowice',
+            title:"Polish food shop with pierogi",
+            description:"Polish food shop with pierogi"
         }
     );
     var idx = lunr(function() {
