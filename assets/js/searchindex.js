cc3d5a2281f3826f229fe522f6f1dcc272b965c6
$env:op_build_user <$env:op_build_user_email>
2019-11-27 12:05:31 +0000

    Commit from AppVeyor

--- assets/js/searchindex.js
+++ assets/js/searchindex.js
@@ -10,20 +10,6 @@ var searchModule = function() {
     a(
         {
             id:0,
-            title:"Polish food shop pierogi",
-            content:"pierogi",
-            description:"Polish food shop pierogi",
-            tags:'Krakow Bronowice Food Food'
-        },
-        {
-            url:'/BigDoorWyamBlog/services/a-food-shop-in-bronowice',
-            title:"Polish food shop with pierogi",
-            description:"Polish food shop with pierogi"
-        }
-    );
-    a(
-        {
-            id:1,
             title:"ab",
             content:"additional search tags",
             description:"Best Dave Car Rental Krakow Bronowice",
@@ -37,7 +23,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:2,
+            id:1,
             title:"John Hair Salon",
             content:"Hair Salon",
             description:"best John Hair Salon Krakow Centrum",
@@ -47,6 +33,20 @@ var searchModule = function() {
             url:'/BigDoorWyamBlog/services/johns-hair-salon',
             title:"John's Hair Salon",
             description:"The best John's Hair Salon in Krakow Centrum"
+        }
+    );
+    a(
+        {
+            id:2,
+            title:"Polish food shop pierogi",
+            content:"pierogi",
+            description:"Polish food shop pierogi",
+            tags:'Krakow Bronowice Food Food'
+        },
+        {
+            url:'/BigDoorWyamBlog/services/a-food-shop-in-bronowice',
+            title:"Polish food shop with pierogi",
+            description:"Polish food shop with pierogi"
         }
     );
     a(
